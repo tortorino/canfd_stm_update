42fe82bd4bfd5a9e64972c526e86c30583450717
tortorino <flammable.d@gmail.com>
2024-10-07 10:05:49 +0200

    Refactor to use COMMANDS dictionary for sending CAN messages
    Modified `canfd_load.py` to leverage a new `COMMANDS` dictionary for CAN command codes. Updated `send_command` method to a more generic `send_message` function, and adjusted relevant functions to use the standardized command structure. This enhances code readability and maintainability.

--- canfd_load.py
+++ canfd_load.py
@@ -7,10 +7,22 @@ class STM32CANFDBootloader:
     def __init__(self, interface='can0'):
         self.bus = can.interface.Bus(channel=interface, bustype='socketcan', fd=True)
         self.TIMEOUT = 1.0
+        self.COMMANDS = {
+            'get': 0x00,
+            'get_version': 0x01,
+            'get_id': 0x02,
+            'read_memory': 0x11,
+            'go': 0x21,
+            'write_memory': 0x31,
+            'erase_memory': 0x44,
+            'write_protect': 0x63,
+            'write_unprotect': 0x73,
+            'readout_protect': 0x82,
+            'readout_unprotect': 0x92
+        }
 
-    def send_command(self, command, data=None):
-        msg = can.Message(arbitration_id=0x111, is_extended_id=False, is_fd=True, bitrate_switch=True)
-        msg.data = bytes([command]) + (data if data else b'')
+    def send_message(self, data):
+        msg = can.Message(arbitration_id=0x111, is_extended_id=False, is_fd=True, bitrate_switch=True, data=data)
         self.bus.send(msg)
 
         response = self.bus.recv(self.TIMEOUT)
@@ -19,7 +31,7 @@ class STM32CANFDBootloader:
         return False
 
     def get_command(self):
-        if not self.send_command(0x00):
+        if not self.send_message(bytes([self.COMMANDS['get']])):
             return None
 
         num_commands = self.bus.recv(self.TIMEOUT).data[0]
@@ -29,26 +41,27 @@ class STM32CANFDBootloader:
         return version, commands
 
     def erase_memory(self, start_page, num_pages):
-        data = struct.pack('>HH', start_page, num_pages)
-        return self.send_command(0x44, data)
+        data = bytes([self.COMMANDS['erase_memory']]) + struct.pack('>HH', start_page, num_pages)
+        return self.send_message(data)
 
     def write_memory(self, address, data):
         addr_bytes = struct.pack('>I', address)
         length = len(data) - 1
-        self.send_command(0x31, addr_bytes + bytes([length]))
+        cmd_data = bytes([self.COMMANDS['write_memory']]) + addr_bytes + bytes([length])
+        if not self.send_message(cmd_data):
+            return False
 
         chunks = [data[i:i+64] for i in range(0, len(data), 64)]
         for chunk in chunks:
-            msg = can.Message(arbitration_id=0x111, is_extended_id=False, is_fd=True, bitrate_switch=True)
-            msg.data = chunk
-            self.bus.send(msg)
+            if not self.send_message(chunk):
+                return False
 
         response = self.bus.recv(self.TIMEOUT)
         return response and response.data[0] == 0x79  # ACK
 
     def go_command(self, address):
         addr_bytes = struct.pack('>I', address)
-        return self.send_command(0x21, addr_bytes)
+        return self.send_message(bytes([self.COMMANDS['go']]) + addr_bytes)
 
     def upload_firmware(self, file_path, start_address):
         with open(file_path, 'rb') as f:
